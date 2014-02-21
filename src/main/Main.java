0e03052527b7d41b114b8757aa2cb3a0e666363f
romascom <matthew.romasco@gmail.com>
2014-02-21 11:11:58 -0800

    Added ability to test all files in the working directory

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import lexer.Lexer;
+import java.io.File;
 //import lexer.Tag;
 //import lexer.Token;
 //import lexer.Word;
 import parser.*;
+
 //import java.io.*;
 
 public class Main {
@@ -15,44 +17,45 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		System.err.println("START the program");//debug
-		//String filename = args[0];
+		System.err.println("START the program");// debug
+		// String filename = args[0];
 		Lexer lex = new Lexer();
 
-		System.out.println("Hello World!");//debug
+		System.out.println("Hello World!");// debug
 		// TODO: Argument checking
-		for (String path: args) {
-			try {
-				// Reassign the standard input stream to be a file
-				System.err.println("The file being opened is " + path);//debug
-				System.setIn(new FileInputStream(path));
-				
-				Parser parser = new Parser(lex);
-				
-				/*
-				// Print lexer output
-				Token tok;
-				while (true) {
-					tok = lex.scan();
-					if (tok == null) {
-						System.err.println("Invalid lexeme");
-						break;
-					}
-					if (tok.equals(Word.eof)) {
-						System.err.println("Reached EOF");
-						break;
-					}
-					System.out.println("<" + tok.tag + ", " + tok.lexeme + ">");
-				}*/
 
-			} catch (FileNotFoundException e) {
-				// TODO
-				System.err.println("Caught FileNotFoundException: "
-						+ e.getMessage());
-			} catch (IOException e) {
-				// TODO
-				System.err.println("Caught IOException: " + e.getMessage());
-			}	
+		System.err.println("args[0] is " + args[0]);// debug
+
+		try {
+			if (args[0].equals("-x")) {
+				File dir = new File("."); // TODO allow user to specify the
+											// directory
+				for (File child : dir.listFiles()) {
+					if (child.isFile() && child.getPath().endsWith(".txt")) {
+						System.err.println("The file being opened is "
+								+ child.getPath());// debug
+						System.setIn(new FileInputStream(child.getPath()));
+
+						Parser parser = new Parser(lex);
+					}
+				}
+			} else {
+				for (String path : args) {
+					// Reassign the standard input stream to be a file
+					System.err.println("The file being opened is " + path);// debug
+					System.setIn(new FileInputStream(path));
+
+					Parser parser = new Parser(lex);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO
+			System.err.println("Caught FileNotFoundException: "
+					+ e.getMessage());
+		} catch (IOException e) {
+			// TODO
+			System.err.println("Caught IOException: " + e.getMessage());
 		}
+
 	}
 }
